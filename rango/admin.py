88c14405e7c70534ac04e7ed88ce605288f27c13
Zhaoyi Wang <2572256w@student.gla.ac.uk>
2021-02-08 04:41:01 +0800

    first commit

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from rango.models import Category,Page
 
-admin.site.register(Category)
-admin.site.register(Page)
+# admin.site.register(Category)
+# admin.site.register(Page)
+
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'category', 'title','url')
+    list_per_page = 20
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'views','likes')
+    list_per_page = 20
+
+admin.site.register(Page,PageAdmin)
+admin.site.register(Category,CategoryAdmin)
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -21,7 +21,6 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -124,7 +123,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATICFILES_DIR = [STATIC_DIR, ]
+STATICFILES_DIRS = [STATIC_DIR, ]
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = MEDIA_DIR
